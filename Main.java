79c9110da21ca7e7b0734952cababf1021247230
Daniel Sharifi <danielsharifi@outlook.com>
2019-02-10 23:58:01 +0100

    Brief start

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+class Main {
+    int[]n = {1000, 10000, 100000, 1000000, 10000000, 100000000};
+    int[]k = {20, 100};
+
+    
+    public static void main(String[] args) {
+
+        System.out.println("ja");
+    }
+
+
+}
